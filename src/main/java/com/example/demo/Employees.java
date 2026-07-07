24a4d258c0d1790fdb213906ed6eb52fe3239f09
Graham Robert <grahamrobert2004@gmail.com>
2026-07-08 00:28:22 +0200

    Added get method to retreive employee list | Set to init if null

--- src/main/java/com/example/demo/Employees.java
+++ src/main/java/com/example/demo/Employees.java
@@ -1,5 +1,17 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//Storage class for Employee objects
 public class Employees {
-    
+    private List<Employee> empList;
+
+    //get emp list - init if null
+    public List<Employee> getEmpList() {
+        if (empList == null) { 
+            empList = new ArrayList<>();
+        }
+        return empList;
+    }
 }
